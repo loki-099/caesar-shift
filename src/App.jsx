730133df46f594e35d82dc81566594a9d6a19643
loki <loidatolentino358@gmail.com>
2023-11-22 23:08:10 +0800

    configure file path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from './components/Main';
 
 const App = () => {
   return (
-    <div className="bg-[url('./assets/background.jpg')] w-full h-screen  bg-center bg-no-repeat bg-cover flex items-center justify-center">
+    <div className="bg-[url('/assets/background.jpg')] w-full h-screen  bg-center bg-no-repeat bg-cover flex items-center justify-center">
       <Main></Main>
     </div>
   )
